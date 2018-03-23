7b8eb6ccb4e6ba225a4c6fb488d72a42429058df
Tim Luo <Timluo99@yahoo.com>
2018-03-23 11:53:41 +1100

    Add files via upload

---
+++ tut2/project_dir/hello-world.py
@@ -0,0 +1,4 @@
+print("This is a git branch")
+print("This is branch dev")
+print("This is a git branch");
+print("This is branch master");
---
+++ tut2/project_dir/hello_world.py
@@ -0,0 +1,6 @@
+print('Hello, World!!!')
+<<<<<<< HEAD
+print("this is a master branch")
+=======
+print("this is dev branch")
+>>>>>>> dev
